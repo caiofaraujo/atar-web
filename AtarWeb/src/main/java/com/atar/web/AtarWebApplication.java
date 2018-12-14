590d9fe6ea8ff20e97e24cc2189600bc0b23c5e5
Caio Fernandes <caiofaraujo21@hotmail.com>
2018-12-13 23:35:40 -0200

    Commit inicial dos arquivos do projeto.

---
+++ AtarWeb/src/main/java/com/atar/web/AtarWebApplication.java
@@ -0,0 +1,15 @@
+package com.atar.web;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtarWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtarWebApplication.class, args);
+	}
+
+}
+
