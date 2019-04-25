a81e8d1d29ad3e5e6da7f8f8c29b194c2b796954
Caio Fernandes <caiofaraujo21@hotmail.com>
2019-04-24 23:01:38 -0300

    Adicionando campo de pesquisa nas telas de listagem (filtro).

--- AtarWeb/src/main/resources/static/js/controller/controllers-servico.js
+++ AtarWeb/src/main/resources/static/js/controller/controllers-servico.js
@@ -163,6 +163,26 @@ app.controller('ServicoCRUDCtrl', ['$scope', '$http', 'ServicoCRUDService','Clie
         $scope.getAllServicos();
 
         $scope.getAllEquipamentos();
+        
+        $scope.sortField = undefined;
+        $scope.reverse = false;
+
+  	 $scope.isSortUp = function(fieldName){
+  	 	return $scope.sortField === fieldName && !$scope.reverse;
+  	 };
+  	 $scope.isSortDown = function(fieldName){
+  	 	return $scope.sortField === fieldName && $scope.reverse;
+  	 };
+  	 //order data
+  	 $scope.sort = function(fieldName){
+  	 	if ($scope.sortField === fieldName) {
+  	 		$scope.reverse = !$scope.reverse;
+  	 	}else{
+  	 		$scope.sortField = fieldName;
+  	 		$scope.reverse = false;
+
+  	 	};
+  	 }
 
     }]);
 
--- AtarWeb/src/test/java/com/atar/web/repositories/ServicoRepositoryTest.java
+++
@@ -1,5 +0,0 @@
-package com.atar.web.repositories;
-
-public class ServicoRepositoryTest {
-
-}
